59b973c91614dc07abf74b33506864566dc4eb3f
1XsteP1 <forroyalee2@gmail.com>
2020-02-26 20:18:46 +0200

    feat: add solution

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -1,7 +1,15 @@
-const MODERN_ACTIVITY= 15; 
+const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
-module.exports = function dateSample(/* sampleActivity */) {
-  throw 'Not implemented';
-  // remove line with error and write your code here
+module.exports = function dateSample(sampleActivity) {
+  if (typeof (sampleActivity) != 'string'
+  || /[a-zA-Z]/g.test(sampleActivity)
+  || sampleActivity <= 0
+  || sampleActivity > MODERN_ACTIVITY) {
+    return false
+  }
+  let activity = parseFloat(sampleActivity)
+  let result = Math.ceil(Math.log(MODERN_ACTIVITY / activity) * HALF_LIFE_PERIOD / 0.693)
+
+  return Math.abs(result)
 };
